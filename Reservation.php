d69cd077c02cb154beccc07fbd311195ee61dfa8
Christophe Simon <c.christophesimon@gmail.com>
2017-09-26 16:02:44 +0200

    Add $page_name Reservation.php

--- Reservation.php
+++ Reservation.php
@@ -1,11 +1,6 @@
 <!Doctype html>
 <html>
-    <Head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="css/style.css" type="text/css" />
-        <link rel="icon" type="image/png" href="ressources/logo.png" />
-        <title> BryanAir <?php echo ($page_name)?> </title>
-    </Head>
+<?php $page_name = "Reservation" ?>   
     <body>
 
     </body/>
