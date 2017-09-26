dad0014645d69c5b34bc6f09612ffad6dbb5ac2c
Christophe Simon <c.christophesimon@gmail.com>
2017-09-26 15:51:54 +0200

    Head in php

--- Reservation.php
+++ Reservation.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8" />
         <link rel="stylesheet" href="css/style.css" type="text/css" />
         <link rel="icon" type="image/png" href="ressources/logo.png" />
-        <title> BryanAir Reservation </title>
+        <title> BryanAir <?php echo ($page_name)?> </title>
     </Head>
+    <body>
+
+    </body/>
 </html>
