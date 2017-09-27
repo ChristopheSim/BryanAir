6e6f2cb5cc24e2471b8534faf92cd450dac737da
Guillaume <guillaumedemoff@gmail.com>
2017-09-27 16:21:25 +0200

    HTML generator

--- controler_home.php
+++ controler_home.php
@@ -1,17 +1,5 @@
 <?php
-echo <<<EOS
-<!DOCTYPE html>
-<html>
-EOS;
-include "templates/head.html";
+include "utils.php";
 
-echo "<body>";
-
-include "templates/header.html";
-include "templates/home.html";
-include "templates/footer.html";
-
-echo <<<EOS
-    </body>
-</html>
-EOS;
+echo buildHTML("home");
+?>
