bbebea395c390f3a4d07d4ddde1e9b043186dc82
Guillaume <guillaumedemoff@gmail.com>
2017-10-25 10:51:16 +0200

    add client and flight classes

---
+++ Client.php
@@ -0,0 +1,42 @@
+<?php
+    class Client
+    {
+        private $last_name;
+        private $first_name;
+        private $age;
+        private $Flights = array();
+        
+        function __construct()
+        {
+            
+        }
+
+        function setLastName($last_name)
+        {
+            $this->last_name = $last_name;
+        }
+
+        function setFirstName($first_name)
+        {
+            $this->first_name = $first_name; 
+        }
+
+        function setAge($age)
+        {
+            $this->age = $age;
+        }
+
+        function addReservation($Flight)
+        {
+            array_push($this->Flights, $Flight)
+        }
+
+        function cancelReservation()
+        {
+
+        }
+
+        
+
+
+    }
