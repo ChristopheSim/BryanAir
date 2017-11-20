4304b9079f3dab963feed1ec3be84281833438b9
Guillaume <guillaumedemoff@gmail.com>
2017-11-20 13:38:40 +0100

    Redirect when no destination selected

--- controler_detail.php
+++ controler_detail.php
@@ -1,13 +1,20 @@
 <?php
+
+if($_POST["destination"] == "null")
+{
+    echo "coisdflksjf";
+    header('Location: ./reservation');
+    
+}
+
 $flights = unserialize($_SESSION["flights"]);
 $flight = $flights[$_POST["destination"]];
 $tags = array("title" => "Detail");
 
+
 if ($flight->getAvailableSeat() >= $_POST["NumberOfPassengers"])
 {
     $flights[$_POST["destination"]]->removeSeats($_POST["NumberOfPassengers"]);
-    echo $flights[$_POST["destination"]]->getAvailableSeat();
-    
     $clients = array();
     $_SESSION["clients"] = serialize($clients); 
     $_SESSION["reservation"] = array("total_passenger" => $_POST["NumberOfPassengers"], "registerd_passenger" => 0);
