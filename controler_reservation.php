1ae04436b6a950e8c4f439e0bf50617dbbdd9c37
Guillaume <guillaumedemoff@gmail.com>
2017-11-20 11:00:48 +0100

    Make flight list from fligths object

--- controler_reservation.php
+++ controler_reservation.php
@@ -2,7 +2,19 @@
 
 $baraki = new Flight("baraki" , "15/11/2017 09:20","BR987");
 $torremolinos = new Flight("Torremolinos" , "15/11/2017 09:20","BR3657");
-$_SESSION["flights"] = serialize(array($baraki, $torremolinos));
-   
-echo buildHTML("reservation");
+$flights = array($baraki, $torremolinos);
+$_SESSION["flights"] = serialize($flights);
+
+$destinationsHTML = "";
+
+foreach($flights as $flight)
+{
+$destinationsHTML .= "<option>";
+$destinationsHTML .= $flight->getDestination();
+$destinationsHTML .= "</option>";
+$destinationsHTML .= "/n";
+}
+
+$tags = array("destinations" => $destinationsHTML);
+echo buildHTML("reservation", $tags);
 ?>
