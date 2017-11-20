44fc3d335fe3120e4c1c64a91eda937d84646236
Guillaume <guillaumedemoff@gmail.com>
2017-11-20 11:27:57 +0100

    Get correct fligth in controler detail

--- controler_detail.php
+++ controler_detail.php
@@ -1,14 +1,15 @@
 <?php
-
-
-$flight = unserialize($_SESSION["flights"])[0];
+$restriction = 
+$flight = array_filter(unserialize($_SESSION["flights"]),function($flight){return $flight -> getDestination() == $_POST["destination"];})[0];
 
 if ($flight->getAvailableSeat() >= $_POST["NumberOfPassengers"])
 {
     $clients = array();
     $_SESSION["clients"] = serialize($clients); 
     $_SESSION["reservation"] = array("total_passenger" => $_POST["NumberOfPassengers"], "registerd_passenger" => 0);
-    echo buildHTML("detail");
+
+    $tags = array("title" => "Detail");
+    echo buildHTML("detail", $tags);
 }
 
 
