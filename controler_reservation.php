f5d8a6486f6622ef2845d6419b740320a40896a3
Guillaume <guillaumedemoff@gmail.com>
2017-11-27 13:37:12 +0100

    Get destination from DB

--- controler_reservation.php
+++ controler_reservation.php
@@ -1,17 +1,34 @@
 <?php
 
-$flights = unserialize($_SESSION["flights"]);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "BryanAir";
 
-
-$destinationsHTML = "";
-$i = 0;
-foreach($flights as $flight)
-{
-$destinationsHTML .= sprintf("<option value= %d>", $i);
-$destinationsHTML .= $flight->getDestination();
-$destinationsHTML .= "</option>";
-$i++;
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+
+$sql = "SELECT IATA, name FROM airport";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+
+    $destinationsHTML = "";
+    while($flight = mysqli_fetch_assoc($result)) {
+        $destinationsHTML .= sprintf("<option value= %s>", $flight["IATA"]);
+        $destinationsHTML .= sprintf("%s - (%s)", $flight["name"], $flight["IATA"]);
+        $destinationsHTML .= "</option>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
 
 $tags = array("destinations" => $destinationsHTML);
 $tags["title"] = "Reservation";
