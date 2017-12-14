2f6d2d1ffc2775e5e90a4f53f3307f0f97345160
Guillaume <guillaumedemoff@gmail.com>
2017-12-14 22:10:39 +0100

    add function to load flight info and av seats

--- controler_detail.php
+++ controler_detail.php
@@ -39,71 +39,20 @@ if (!$conn) {
 }
 
 // get outbound flight data
-$sql = Sprintf( "SELECT seats, number FROM flights WHERE departure ='%s' && arrival = '%s' ", $_POST["departure"], $_POST["arrival"]);
-$result = mysqli_query($conn, $sql);
-$flight;
-
-if (mysqli_num_rows($result) > 0) {
-    
-    $flight = mysqli_fetch_assoc($result);
-    $obFlight = new Flight($_POST["departure"], $_POST["arrival"], $flight["number"], $flight["seats"]);
-    // count number of taken seat for outbound
-    $sql = sprintf("SELECT COUNT(client) AS taken_seats FROM reservation WHERE flight = '%s'", $obFlight->getNumber());
-    $result = mysqli_query($conn, $sql);
-    if (mysqli_num_rows($result) > 0) 
-    {
-        $av_seats = $obFlight->getSeats() - mysqli_fetch_assoc($result)["taken_seats"];
-    } 
-    else 
-    {
-        $_SESSION['error'] = '0 results';
-        header('Location: BryanAir/reservation');
-        die();
-    }
-} 
-else {
-    $_SESSION['error'] = '0 results';
-    header('Location: /BryanAir/reservation');
-    die();
-}
-
-//crer une fonction charge flight avec en param un flight
+$obFlight = loadFlightSeatsNumber($conn, $_POST["departure"], $_POST["arrival"]);
+$av_seats= getAvailableSeats($conn, $obFlight);
 
 // get return flight data
 if(isset($_POST["return"]) == "true")
 {
-    $sql = Sprintf( "SELECT seats, number FROM flights WHERE departure ='%s' && arrival = '%s' ", $_POST["arrival"], $_POST["departure"]);
-    $result = mysqli_query($conn, $sql);
-    $flight;
-    if (mysqli_num_rows($result) > 0) 
-    {
-        $return_flight = mysqli_fetch_assoc($result);
-        $rFlight = new Flight($_POST["arrival"], $_POST["departure"], $return_flight["number"], $return_flight["seats"]);
-        // count number of taken seat for outbound
-        $sql = sprintf("SELECT COUNT(client) AS taken_seats FROM reservation WHERE flight = '%s'", $rFlight->getNumber());
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) 
-        {
-            $av_seats_return = $rFlight->getSeats() - mysqli_fetch_assoc($result)["taken_seats"];
-        } 
-        else 
-        {
-            $_SESSION['error'] = '0 results'; 
-            header('Location: BryanAir/reservation');
-            die();
-        }
-    } 
-    else 
-    {
-        $_SESSION['error'] = '0 results';
-        header('Location: BryanAir/reservation');
-        die();
-    }
+   $rFlight = loadFlightSeatsNumber($conn, $_POST["arrival"], $_POST["departure"]);
+   $av_seats_return = getAvailableSeats($conn, $rFlight);
 }
 
 mysqli_close($conn);
 
 $reservation = new Reservation($_POST["passengers_nb"], 0, $_POST["email"], $obFlight, $rFlight);
+
 
 if($av_seats < $reservation->getTotalPassenger())
 {
